8765a7ecdf4f6efa3dd4f6e121b032c725c5e799
Namrata Kapoor <Namrata Kapoor>
2016-01-30 16:22:14 -0700

    initial commit

---
+++ conditional.py
@@ -0,0 +1,11 @@
+x=100
+day="Friday"
+if x<10:
+    print("The day is"+ day +\
+         ". Its a great day")
+elif day=="Friday":
+    print("i love fridays")
+else:
+    print("i dont think this will happen")
+    
+    
---
+++ iteration.py
@@ -0,0 +1,5 @@
+books=["tale of two cities", "harry potter","lord of the rings"]
+num=range(100)
+
+for book in books:
+    print(book)
